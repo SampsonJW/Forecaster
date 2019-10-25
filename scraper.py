92ef3a1ae8340e66cbde384b83cfb7361411e3f1
sampsonJW <sampsondraw@gmail.com>
2019-10-25 15:58:44 +1300

    Added Gui to search weather by city

--- scraper.py
+++ scraper.py
@@ -1,24 +1,38 @@
 import json
 import requests
-import tkinter
+import tkinter as tk
 
 APIcall = 'http://api.openweathermap.org/data/2.5/weather?q={},NZ&mode=json&units=metric&cnt=5&APPID=eaf254b78fb79221fb9747d92329ac53'
+
+root = tk.Tk()
+root.minsize(720,480)
+root.title('Weather')
+root.configure(background="white")
+frame = tk.Frame(root)
+frame.configure(background="white")
+frame.pack()
+
+label = tk.Label(frame, background = "white", text = 'Enter your city:').pack()
+cityEntry = tk.Entry(frame,background = "white")
+cityEntry.pack()
+
+def show_weather(weatherDict):
+    tk.Label(frame, background = "white", text = "Conditions: " + weatherDict['rain']).pack()
+    tk.Label(frame, background = "white", text = "Temperature: " + str(weatherDict['temp'])).pack()
+
 
 class Test(object):
     def __init__(self, data):
 	    self.__dict__ = json.loads(data)
 
 def get_weather():
-    weather = requests.get(APIcall.format(input()))
+    
+    weather = requests.get(APIcall.format(cityEntry.get()))
     raining = Test(weather.text).weather[0]['description']
     temp = Test(weather.text).main['temp']
-    show_weather(temp)
+    weathDict = {'temp': temp, 'rain': raining}
+    show_weather(weathDict)
 
-def show_weather(str):
-    window = tkinter.Tk()
-    window.minsize(150, 100)
-    window.title("Weather")
-    label = tkinter.Label(window, text = str).pack()
-    window.mainloop()
+b = tk.Button(frame, background = "white", text='Go', command = get_weather).pack()
 
-get_weather()
+root.mainloop()
